d5b801edec2702b3e332c0bbefcdb907e815696c
nirbenator <nir.benatar@gmail.com>
2019-01-13 18:40:18 +0200

    fixed

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+# Install sshpass for quiet transfer of ssh key
+apt-get install sshpass -f
+# Generate RSA pair
+su vagrant -c 'ssh-keygen -t rsa -q -N "" -f /home/vagrant/.ssh/id_rsa'
+
+# chowning to vagrant
+chown -R vagrant:vagrant /home/vagrant/.ssh/*
+
+# Allow SSH connection without host key checking
+echo "Host server2" >> ~vagrant/.ssh/config
+echo "  StrictHostKeyChecking no" >> ~vagrant/.ssh/config
